f454ebece5639b3e9dfee4883c7d4e5f771663b8
gokhek <evgeniy.goh@gmail.com>
2022-03-24 23:00:17 +0500

    new bot for rts

---
+++ main.py
@@ -0,0 +1,20 @@
+import telebot
+
+token='5290181621:AAHKDdvI5-b8c0TvdZOqmz5IysOVByxye8A'
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+  bot.send_message(message.chat.id,"Привет ✌️ ")
+
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    if message.text == "привет":
+        bot.send_message(message.chat.id, "И тебе привет!")
+    elif message.text == "как дела":
+        bot.send_message(message.chat.id, "Отлично! А у тебя?")
+    else:
+        bot.send_message(message.chat.id, "Извините, я вас не понимаю")
+
+bot.polling(none_stop = True)
